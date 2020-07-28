e415ad2a96ca65cb964063cbcfb2765cf47e3645
demi.jiang <demi.jiang@myob.com>
2020-07-28 10:05:01 +1000

    make test run successfully on chrome browser

--- seleniumgrid-docker/src/test/java/TestClass.java
+++ seleniumgrid-docker/src/test/java/TestClass.java
@@ -14,12 +14,12 @@ public class TestClass {
     @Parameters({"Port"})
     @BeforeClass
     public void initiateDriver(String Port) throws MalformedURLException {
-        if(Port.equalsIgnoreCase("9001"))
-        {
-            driver = new RemoteWebDriver(new URL("http:localhost:4444/wd/hub"), DesiredCapabilities.chrome());
-            driver.manage().window().maximize();
-        }
-        else if(Port.equalsIgnoreCase("9002")){
+//        if(Port.equalsIgnoreCase("9001"))
+//        {
+//            driver = new RemoteWebDriver(new URL("http:localhost:4444/wd/hub"), DesiredCapabilities.chrome());
+//            driver.manage().window().maximize();
+//        }
+         if(Port.equalsIgnoreCase("9002")){
             driver = new RemoteWebDriver(new URL("http:localhost:4444/wd/hub"), DesiredCapabilities.firefox());
             driver.manage().window().maximize();
         }
--- seleniumgrid-docker/src/test/java/base/BaseTests.java
+++ seleniumgrid-docker/src/test/java/base/BaseTests.java
@@ -1,25 +1,58 @@
 package base;
 
-import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+import pages.HomePage;
+import pages.LoginPage;
+import pages.SecureAreaPage;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import static org.testng.Assert.assertTrue;
 
 public class BaseTests {
     private WebDriver driver;
+    protected HomePage homePage;
 
-    public void setUp(){
-        System.setProperty("webdriver.chrome.driver", "resources/chromedriver");
-        driver = new ChromeDriver();
+//    @BeforeClass
+//    public void setUp(){
+//        System.setProperty("webdriver.chrome.driver", "resources/chromedriver");
+//        driver = new ChromeDriver();
+//        driver.get("https://the-internet.herokuapp.com/");
+//
+//        homePage = new HomePage(driver);
+//
+//    }
+
+
+    @Parameters({"Port"})
+    @BeforeClass
+    public void initiateDriver(String Port) throws MalformedURLException {
+        if(Port.equalsIgnoreCase("9001"))
+        {
+            driver = new RemoteWebDriver(new URL("http:localhost:4444/wd/hub"), DesiredCapabilities.chrome());
+            driver.manage().window().maximize();
+        }
+        else if(Port.equalsIgnoreCase("9002")){
+            driver = new RemoteWebDriver(new URL("http:localhost:4444/wd/hub"), DesiredCapabilities.firefox());
+            driver.manage().window().maximize();
+        }
+
         driver.get("https://the-internet.herokuapp.com/");
+        homePage = new HomePage(driver);
+    }
 
-        driver.manage().window().setSize(new Dimension(375, 812));
 
-        System.out.println(driver.getTitle());
+    @AfterClass
+    public void tearDown(){
         driver.quit();
     }
 
-    public static void main(String args[]){
-        BaseTests test = new BaseTests();
-        test.setUp();
-    }
 }
